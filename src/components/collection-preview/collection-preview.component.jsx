02884b18bccdfc2744b20bed1b81f294f39d7cd5
girish <girish.natashankar@publicissapient.com>
2022-01-31 13:28:00 +0530

    message

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,9 +8,9 @@ const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title}</h1>
         <div className="preview">
-            {items.filter((item, index) => index < 4).map(({id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps}/>
-              ))
+            {items.filter((item, index) => index < 4).map((item) => (
+                <CollectionItem key={id} item={item} />
+            ))
             }
         </div>
     </div>
